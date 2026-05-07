d020ec42db3bce40ad81b9770b7c5db0a6aa0994
Celine <161593235+scholasticaCeline@users.noreply.github.com>
2026-05-07 07:35:28 +0700

    Update page.tsx

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -49,7 +49,7 @@ export default function CatalogPage() {
           <h1 className="text-5xl font-black mb-4 tracking-tight text-center md:text-left">The Cookie Catalog</h1>
           <p className="text-zinc-400 text-xl max-w-2xl font-medium text-center md:text-left">
             All our cookies are baked in small batches to ensure the highest quality. 
-            Every <span className="text-white font-bold">2 cookies</span> are priced at <span className="text-white font-bold">15.000</span>.
+            Every <span className="text-white font-bold">2 cookies</span> are priced at <span className="text-white font-bold">15.000. Individual cookeis are priced at 10.000.</span>.
           </p>
         </div>
       </section>
